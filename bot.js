c91d769a224f2f1446e1333b44d395d62cf90c50
Joseph Hodes <jojoho70@gmail.com>
2021-04-14 11:33:41 -0400

    added firbase config with env

--- bot.js
+++ bot.js
@@ -4,11 +4,20 @@ const fs = require('fs')
 
 
 const x = require('dotenv').config();
-
-const apikey = process.env.Api_key;
+const apikey = process.env.apikey;
 const client = new Discord.Client();
 const prefix = "!";
 const serverid = "";
+
+var firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.measurementId
+};
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
