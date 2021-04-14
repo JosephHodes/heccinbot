b22e00620d4f1721ccab307b4eb6a660ce62374a
Joseph Hodes <jojoho70@gmail.com>
2021-04-14 12:42:26 -0400

    optimized a bit

--- bot.js
+++ bot.js
@@ -1,10 +1,8 @@
 const Discord = require('discord.js');
-const path = require('path')
-const fs = require('fs');
-const { getPriority } = require('os');
 
 
-const x = require('dotenv').config();
+
+require('dotenv').config();
 const apikey = process.env.apikey;
 const client = new Discord.Client();
 const prefix = "!";
@@ -23,12 +21,15 @@ var firebaseConfig = {
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     const x = client.guilds.cache.map(guil => guil.id)
-    client.guilds.fetch(x[0]).then(man => {
-        man.fetchBans().then(banned => {
-            let list = banned.map(user => user.tag).join('\n');
-            console.log(list);
-        })
-    }).catch(err => console.log(err))
+    x.forEach(element => {
+        client.guilds.fetch(element).then(man => {
+            man.fetchBans().then(banned => {
+                let list = banned.map(user => user.tag).join('\n');
+                console.log(list);
+            })
+        }).catch(err => console.log(err))
+    });
+
 })
 
 
