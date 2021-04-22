4fb48c3c1d97e9609127dff6835f782b30c109c6
Joseph Hodes <jojoho70@gmail.com>
2021-04-21 23:35:14 -0400

    checking if ban already exists

--- bot.js
+++ bot.js
@@ -13,6 +13,8 @@ client.on("guildBanAdd", (ban,user )=> {
     console.log(x)
     x.forEach(guildholding=>{
         if(!(ban.id===guildholding)){
+            if(!(ban.fetchBans().then(mann =>{mann.forEach(mmm=>{if(mmm.user.username===user.username){return true} })
+            return false})))
             client.guilds.fetch(guildholding).then(eee=>eee.members.ban(user.id)).then(console.log("ban successful the user was: " +user.username)).catch(err=>console.log(err))
         }
     })
