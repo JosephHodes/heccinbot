9b1ceace9662e78693193fc3125256da5e733adf
Joseph Hodes <jojoho70@gmail.com>
2021-04-22 11:17:58 -0400

    finale push for today

--- bot.js
+++ bot.js
@@ -9,12 +9,11 @@ const client = new Discord.Client();
 client.on("ready",()=>{console.log(`Logged in as ${client.user.tag}!`);
 
 const x = client.guilds.cache.map(guil => guil.id);
-
-console.log(x)});
+});
 
 client.on("guildBanAdd", (ban,user )=> {const x = client.guilds.cache.map(guil => guil.id);
 
-console.log(x);
+
 
  x.forEach(guildholding=>{if(!(ban.id===guildholding)){if((ban.fetchBans().then(mann =>{mann.forEach(mmm=>{if(mmm.user.username===user.username){return false} });
 
@@ -27,10 +26,6 @@ client.login((apikey));
 client.on("guildBanRemove", (guild, user)=>{
 
 const x = client.guilds.cache.map(guil => guil.id);
-
-console.log(x);
-
  x.forEach(guildholding=>{if(!(guild.id===guildholding)){if((guild.fetchBans().then(mann =>{mann.forEach(mmm=>{if(mmm.user.username===user.username){return true} });
-
 return false})))client.guilds.fetch(guildholding).then(eee=>eee.members.unban(user.id,{reason:"this is a asyncronous unban across all servers"})).then(console.log("unban successful the user was: " +user.username)).catch(err=> console.log("")) }})
 })
