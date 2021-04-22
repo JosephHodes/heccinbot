a89c4846c5fa894645c39508273bb6e7f9927e13
Joseph Hodes <jojoho70@gmail.com>
2021-04-21 20:57:40 -0400

    working stage sync bans work

--- bot.js
+++ bot.js
@@ -3,42 +3,24 @@ require('dotenv').config();
 
 const apikey = process.env.apikey;
 const client = new Discord.Client();
-const prefix = "!";
-const serverid = "";
 
-var firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.measurementId
-};
-
-const fire = require("firebase").default.initializeApp(firebaseConfig);
-
-client.on("guildBanAdd", ban => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    const x = client.guilds.cache.map(guil => guil.id)
-    
-        
-    x.forEach(ee=>{
-        if(!(ban.id==ee)){
-            client.guilds.fetch(ee).then(eee=>eee.member().ban({reason:"this is a automated ban through the bot"}).then(xx=>console.log("bann successfull")).catch(ree=>{throw ree}))
-        }
-    client.guilds.fetch(ee).then(x=>{
-        x.fetchBans().then(sys=>{
-            console.log(sys)
-           
-        }
-        ).catch(err=>{throw err})})
-    })
-    })
-
-client.on('guildBanAdd', ban => {
-
+client.on("ready",()=>{console.log(`Logged in as ${client.user.tag}!`)
+const x = client.guilds.cache.map(guil => guil.id)
+console.log(x)
 })
+client.on("guildBanAdd", (ban,user )=> {
+    const x = client.guilds.cache.map(guil => guil.id)
+    console.log(x)
+    x.forEach(guildholding=>{
+        if(!(ban.id===guildholding)){
+            client.guilds.fetch(guildholding).then(eee=>eee.members.ban(user.id)).then(console.log("ban successful the user was: " +user.username)).catch(err=>console.log(err))
+        }
+    })
+    })
+
+// client.on('guildBanAdd', ban => {
+
+// })
 
 
 
